fe699effe4b14bd1d9fd81a354d5e1ef97dcaecc
Hanane <s2563420@ed.ac.uk>
2026-06-22 16:33:28 +0100

    fixed setup code paths for colab

--- shared/setup_code.py
+++ shared/setup_code.py
@@ -1,5 +1,16 @@
+import os
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 stroke_data = pd.read_csv("../data/healthcare-dataset-stroke-data.csv")
+
+file_path = "../data/healthcare-dataset-stroke-data.csv"
+
+if not os.path.exists(file_path):
+    # Likely running in Colab or missing repo structure
+    !wget -q https://raw.githubusercontent.com/hanane-issa/python-crash-course/main/data/healthcare-dataset-stroke-data.csv
+    file_path = "healthcare-dataset-stroke-data.csv"
+
+stroke_data = pd.read_csv(file_path)
