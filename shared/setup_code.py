9893e30c8f95676c480419246abd32a174d725ba
Hanane <s2563420@ed.ac.uk>
2026-06-22 17:43:00 +0100

    Fix data loading for Colab, Binder, and Jupyter Book compatibility

--- shared/setup_code.py
+++ shared/setup_code.py
@@ -5,6 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+URL = "https://raw.githubusercontent.com/hanane-issa/python-crash-course/main/data/healthcare-dataset-stroke-data.csv"
+
+stroke_data = pd.read_csv(URL)
 
 url = "https://raw.githubusercontent.com/hanane-issa/python-crash-course/main/data/healthcare-dataset-stroke-data.csv"
 
